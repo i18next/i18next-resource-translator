76f6703c412adfcd77a57b91aec081dbbae8eeb3
Adriano Raiano <adriano@raiano.ch>
2025-02-25 17:36:36 +0100

    update i18next-resource-translator

---
+++ _next/static/b5VCfFzdhFwEjhUnm0GZ_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/845-0dbb0c1c789b1bcf.js","static/chunks/pages/index-01a7c7607ecfe1cb.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/fChMJlOahmibycD0ASdEB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/845-0dbb0c1c789b1bcf.js","static/chunks/pages/index-f755527a4e3c7aa4.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
