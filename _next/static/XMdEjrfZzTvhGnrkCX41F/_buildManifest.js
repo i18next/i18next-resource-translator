c42bedff68288a587b90b9eeb07f9d9433a42c87
Adriano Raiano <adriano@raiano.ch>
2025-03-25 11:17:35 +0100

    update i18next-resource-translator

---
+++ _next/static/XMdEjrfZzTvhGnrkCX41F/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/845-0dbb0c1c789b1bcf.js","static/chunks/pages/index-c2b3afd75c1cd29d.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
